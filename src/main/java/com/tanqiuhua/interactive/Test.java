750d5cade1eb8055d2a0df6af775dabaee1791d9
tantec <tantec@vip.qq.com>
2016-05-17 14:19:53 +0800

    初始化提交

---
+++ src/main/java/com/tanqiuhua/interactive/Test.java
@@ -0,0 +1,17 @@
+package com.tanqiuhua.interactive;
+
+/**
+ * 测试
+ * @author PascalTam
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+
+}
